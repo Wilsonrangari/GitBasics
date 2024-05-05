148857bf69f654acefd76800ea1fa26b062e08a9
wilson rangari <wilson.rangari@gmail.com>
2024-05-05 05:59:18 -0700

    Add addition logic to the code

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,15 @@
 def subtract(a, b):
     return a - b
+def add(a, b):
+    return a + b
+
 
 def main():
     print("Hello World!!")
     a, b = 10, 5
     print(f"Subtraction of {a} - {b} = {subtract(a, b)}")
+    print(f"Additon of {a} + {b} = {add(a,b)}")
+    
 
 if __name__ == '__main__':
     main()
