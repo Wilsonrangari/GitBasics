42199203f0164c9bb20219b097ed7ddadafece50
Wilsonrangari <98660886+Wilsonrangari@users.noreply.github.com>
2024-05-05 07:01:37 -0700

    Multiply (#5)
    * Add multiply logic
    
    * Add divide logic
    
    * Add print
    
    ---------
    
    Co-authored-by: wilson rangari <wilson.rangari@gmail.com>
    Co-authored-by: Saurabh Birari <saurabh.birari@gmail.com>

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,14 @@
+def divide(a, b):
+    return a/b
+
+def multiply(a,b):
+    return a*b
+
 def subtract(a, b):
     return a - b
 def add(a, b):
     return a + b
+
 
 
 def main():
@@ -9,7 +16,8 @@ def main():
     a, b = 10, 5
     print(f"Subtraction of {a} - {b} = {subtract(a, b)}")
     print(f"Additon of {a} + {b} = {add(a,b)}")
-    
+    print(f"Division of {a} / {b} = {divide(a, b)}")
+    print(f"Multiply of {a} * {b} = {multiply(a,b)}")
 
 if __name__ == '__main__':
     main()
